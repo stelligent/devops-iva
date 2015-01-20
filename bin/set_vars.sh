31657d304362192ff51d0c1313819272b53074e8
Dave Tashner <dtashner@singlestoneconsulting.com>
2015-01-20 17:54:53 -0500

    removing twilio secrets

--- bin/set_vars.sh
+++ bin/set_vars.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -v
-echo 'JAVA_OPTS="-Dtwilio.authToken=631e0b6354889bea892f9376255b824e -Dtwilio.sid=AC203eee847735dbdcd1a70461ef323052"' >> /etc/tomcat7/tomcat7.conf
+echo 'JAVA_OPTS="-Dtwilio.authToken=your_auth_token -Dtwilio.sid=your_sid"' >> /etc/tomcat7/tomcat7.conf
